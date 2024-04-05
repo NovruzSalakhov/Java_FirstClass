ea98a5082945391bf79a71ea8878ed170b0277a8
NovruzSalakhov <salakhov-novruz@mail,ru>
2024-04-05 13:50:15 +0300

    added new class for Java

---
+++ src/main/java/novruzsalakhov/com/Main.java
@@ -0,0 +1,18 @@
+package novruzsalakhov.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(15%7);
+
+
+    }
+
+
+}
+
+// Операторы сравнения > < <= >= != ==
+// Арифметические операторы + - * / % ++ --
+// Логические операторы && - "и", || - "или", ! - "отрицание"
+// Операторы присвоения = += -= *=
